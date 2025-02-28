f114fbdcb300844e0f883e7d95b60592b8848998
Waled Khatiz <waledkhatiz@waleds-air.lan>
2025-02-28 12:18:57 +1100

    home page setup without routing

--- ktz-blog/src/App.tsx
+++ ktz-blog/src/App.tsx
@@ -3,9 +3,14 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="w-full h-svh bg-stone-100 dark:bg-stone-800">
+    <div className="w-full h-full min-h-svh bg-stone-100 dark:bg-stone-800">
       <Header />
-      <Outlet />
+      <div className="block mx-auto w-full max-w-[1500px] px-10">
+        <Outlet />
+        <div className="p-5 mt-10 text-black/[0.5] text-sm w-full text-right">
+          Published by Waled Khatiz (ktz-dev)
+        </div>
+      </div>
     </div>
   );
 }
