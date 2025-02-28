84957dfc0aee1a5428e6d9665e33101f8adf3bc4
Waled Khatiz <waledkhatiz99@gmail.com>
2025-02-28 12:34:11 +1100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="w-full h-full min-h-svh bg-stone-100 dark:bg-stone-800">
       <Header />
-      <div className="block mx-auto w-full max-w-[1500px] px-10">
+      <div className="block mx-auto w-full max-w-[1500px] px-3 md:px-10">
         <Outlet />
         <div className="p-5 mt-10 text-black/[0.5] text-sm w-full text-right">
           Published by Waled Khatiz (ktz-dev)
